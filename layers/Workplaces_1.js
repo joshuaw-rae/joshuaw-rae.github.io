30e0f5012d87ec0a28b3d453b9974b225966cc8d
Joshua Rae <joshuaw.rae@gmail.com>
2022-08-16 17:13:06 -0500

    Add files via upload

---
+++ layers/Workplaces_1.js
@@ -0,0 +1,12 @@
+var json_Workplaces_1 = {
+"type": "FeatureCollection",
+"name": "Workplaces_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Position Title": "GIS Seasonal - Natural Resources Management", "Start date": "2022-02-08", "End date": null, "Employer": "Forest Preserve District of Will County", "Job Duty 1": null, "Job Duty 2": null, "Job Duty 3": null, "Location": "New Lenox, IL" }, "geometry": { "type": "Point", "coordinates": [ -88.043468518202147, 41.483471910237192 ] } },
+{ "type": "Feature", "properties": { "Position Title": "Natural Resources Management Technician", "Start date": "2021-05-03", "End date": "2022-02-04", "Employer": "Forest Preserve District of DuPage County", "Job Duty 1": null, "Job Duty 2": null, "Job Duty 3": null, "Location": "West Chicago, IL" }, "geometry": { "type": "Point", "coordinates": [ -88.190850973596483, 41.843465752461924 ] } },
+{ "type": "Feature", "properties": { "Position Title": "IT Services Analyst", "Start date": "2017-09-01", "End date": "2021-05-05", "Employer": "Michigan State University Information Technology", "Job Duty 1": null, "Job Duty 2": null, "Job Duty 3": null, "Location": "East Lansing, IL" }, "geometry": { "type": "Point", "coordinates": [ -84.476736853388616, 42.728770869059097 ] } },
+{ "type": "Feature", "properties": { "Position Title": "Natural Resources Management Intern", "Start date": "2020-05-03", "End date": "2020-09-11", "Employer": "The Conservation Foundation", "Job Duty 1": null, "Job Duty 2": null, "Job Duty 3": null, "Location": "Naperville, IL" }, "geometry": { "type": "Point", "coordinates": [ -88.142702007260695, 41.717694204475428 ] } },
+{ "type": "Feature", "properties": { "Position Title": "Bachelors of Science in Environmental Studies and Sustainability", "Start date": null, "End date": "2021-05-14", "Employer": "Michigan State University", "Job Duty 1": null, "Job Duty 2": null, "Job Duty 3": null, "Location": "East Lansing, IL" }, "geometry": { "type": "Point", "coordinates": [ -84.484256773483978, 42.728088557127556 ] } }
+]
+}
